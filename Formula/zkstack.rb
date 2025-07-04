6561fd93053434d9ae4aa629cdd06c743ef6bdf8
Dustin Brickwood <dustinbrickwood204@gmail.com>
2025-07-03 19:52:17 -0500

    fix: adjust ci

--- Formula/zkstack.rb
+++ Formula/zkstack.rb
@@ -1,5 +1,5 @@
 class Zkstack < Formula
-  desc     "ZKsync Stack CLI - spin up local or prod ZK Stack chains"
+  desc     "ZKsync Stack CLI - spin up ZK Stack chains"
   homepage "https://github.com/matter-labs/zksync-era/tree/main/zkstack_cli"
   url      "https://github.com/dutterbutter/zkstack-cli/archive/refs/tags/v0.1.2.tar.gz"
   sha256   "30abf5b920d69b516ef42898ba285f03b77749b80b9d832bd877644d7d99557e"
